89d26d429140cc4c27944d12e0df5bce85b8ec3c
Kristian Wiklund <githubkristian@snabela.nl>
2020-01-15 23:40:32 +0100

    1:5

--- utils.erl
+++ utils.erl
@@ -1,6 +1,16 @@
 -module(utils).
 -include_lib("eunit/include/eunit.hrl").
--export([hex2b64/1,fxor/2,decryptxor/1]).
+-export([hex2b64/1,fxor/2,decryptxor/1,readfile/1, repxor/2]).
+
+rf(File) ->
+    case file:read_line(File) of
+        {ok, Data} -> [Data | rf(File)];
+        eof        -> []
+    end.
+
+readfile(Filename) ->
+    {ok, File} = file:open(Filename, read),
+    rf(File).
 
 fxor(From, What) ->
     T1 = list_to_integer(From, 16),
@@ -27,7 +37,9 @@ score(String) ->
     P = freq(),
     lists:foldl(fun(C,Acc) ->
 			if C == 0 ->
-				0; % there cannot be any zero in a string
+				-1000; % there cannot be any zero in a string
+			   C > 127 -> % not ascii
+				-1000;
 			   true ->
 				Acc+string:str(P, string:to_upper([C]))
 			end
@@ -43,8 +55,8 @@ decryptxor(Message) ->
 		      {score(T),io_lib:format("~s",[T])} 
 		  end, lists:seq(0,255)),
     Q = lists:keysort(1,P),
-    {_,[Decoded]}=lists:nth(1,lists:reverse(Q)),
-    Decoded.
+    {Score,[Decoded]}=lists:nth(1,lists:reverse(Q)),
+    {Score,Decoded}.
     
 hex2b64(From) ->
     T = binary:encode_unsigned(list_to_integer(From, 16)),
@@ -53,4 +65,23 @@ hex2b64(From) ->
 hex2b64_test() ->
     X=hex2b64("49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d"),
     "SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t" = X.
+
+repxor([M|Essage], [K|Ey], Key)->
+%    io:fwrite("~s ^ ~s ~B\n",[[M],[K],M bxor K]),
+    [io_lib:format("~2.16.0B",[M bxor K]) | repxor(Essage, Ey, Key)];
+repxor([],_,_) ->
+    "";
+repxor(Message,[],Key) ->
+    repxor(Message,Key,Key).
+
+repxor(Message, Key) ->
+    T = repxor(Message, Key, Key),
+    lists:flatten(T).
+    
+    
+
+repxor_test() ->
+    P = [$0|integer_to_list(list_to_integer("0B3637272A2B2E63622C2E69692A23693A2A3C6324202D623D63343C2A26226324272765272a282b2f20430a652e2c652a3124333a653e2b2027630c692b20283165286326302e27282f" ,16),16)],
+    X = repxor("Burning 'em, if you ain't quick and nimble\nI go crazy when I hear a cymbal", "ICE"),
+    P=X.
 
