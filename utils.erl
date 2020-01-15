dfa4a90df2af1ddb7e3849a31ec566f3b601835a
Kristian Wiklund <githubkristian@snabela.nl>
2020-01-15 01:18:35 +0100

    1:3

--- utils.erl
+++ utils.erl
@@ -1,16 +1,51 @@
 -module(utils).
 -include_lib("eunit/include/eunit.hrl").
--export([hex2b64/1,fxor/2]).
+-export([hex2b64/1,fxor/2,findxorkey/1]).
 
 fxor(From, What) ->
     T1 = list_to_integer(From, 16),
     T2 = list_to_integer(What, 16),
     integer_to_list(T1 bxor T2, 16).
 
+
 fxor_test() ->
     P = integer_to_list(list_to_integer("746865206B696420646f6E277420706C6179",16),16),
     K = fxor("1c0111001f010100061a024b53535009181c","686974207468652062756c6c277320657965"),
     P = K.
+
+freq() ->
+    lists:reverse("ETAOINSRHDLUCMFYWGPBVKXQJX").
+
+h2s([A,B|Hex], S)->
+    h2s(Hex,[list_to_integer([A,B],16)|S]);
+h2s(_,S) ->
+    S.
+h2s(Hex) ->
+    lists:reverse(h2s(Hex,"")).
+
+score(String) ->
+    P = freq(),
+    lists:foldl(fun(C,Acc) ->
+			if C == 0 ->
+				0; % there cannot be any zero in a string
+			   true ->
+				Acc+string:str(P, string:to_upper([C]))
+			end
+		end, 0, String).
+			
+
+findxorkey(Message) ->
+    M = h2s(Message),
+    P = lists:map(fun(X) ->
+		      T = lists:map(fun(P) ->
+					    P bxor X
+				    end, M),
+		      {score(T),io_lib:format("~s",[T])} 
+		  end, lists:seq(0,255)),
+    Q = lists:keysort(1,P),
+    {_,[Decoded]}=lists:nth(1,lists:reverse(Q)),
+    Decoded.
+    
 
 hex2b64(From) ->
     T = binary:encode_unsigned(list_to_integer(From, 16)),
